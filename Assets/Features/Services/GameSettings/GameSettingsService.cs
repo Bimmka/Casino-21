b2b54197f7d00c55c42d4a58326e0f2de8e7abe0
Bimmka <bibimmka@gmail.com>
2023-01-21 15:03:47 +0100

    Add bet in game settings

--- Assets/Features/Services/GameSettings/GameSettingsService.cs
+++ Assets/Features/Services/GameSettings/GameSettingsService.cs
@@ -3,10 +3,16 @@ namespace Features.Services.GameSettings
   public class GameSettingsService : IGameSettings
   {
     public GameDifficultType DifficultType { get; private set; }
+    public int CurrentBet { get; private set; }
 
     public void SetType(GameDifficultType type)
     {
       DifficultType = type;
+    }
+
+    public void InitializeBet(int bet)
+    {
+      CurrentBet = bet;
     }
   }
 }
--- Assets/Features/Services/GameSettings/IGameSettings.cs
+++ Assets/Features/Services/GameSettings/IGameSettings.cs
@@ -3,6 +3,8 @@ namespace Features.Services.GameSettings
   public interface IGameSettings
   {
     GameDifficultType DifficultType { get; }
+    int CurrentBet { get; }
     void SetType(GameDifficultType type);
+    void InitializeBet(int bet);
   }
 }
