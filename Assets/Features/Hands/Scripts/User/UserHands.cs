4b00afea4b538258f843cbb80cdc5ad95315a081
Bimmka <bibimmka@gmail.com>
2023-01-22 19:06:03 +0100

    missed

--- Assets/Features/Hands/Scripts/User/UserHands.cs
+++ Assets/Features/Hands/Scripts/User/UserHands.cs
@@ -4,18 +4,6 @@ namespace Features.Hands.Scripts.User
 {
   public class UserHands : BaseHands
   {
-    public bool IsTakingCard { get; private set; }
-
-    public override void TakeCard()
-    {
-      base.TakeCard();
-      IsTakingCard = true;
-    }
-
-    protected override void OnTookCard()
-    {
-      base.OnTookCard();
-      IsTakingCard = false;
-    }
+    
   }
 }
